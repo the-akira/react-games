ccf2005341e0bb7f207c3f8da1ceb96ca3608e28
Gabriel Felippe <gabrielfelippe90@gmail.com>
2022-04-12 14:27:07 -0300

    Links de Imagens Atualizados

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -5,7 +5,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Konami",
     director: "Koji Igarashi",
-    image: { url: "https://static-cdn.jtvnw.net/ttv-boxart/Castlevania%3A%20Symphony%20of%20the%20Night.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/SOTN.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd471816",
@@ -13,7 +13,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Nintendo",
     director: "Takashi Tezuka",
-    image: { url: "https://playclassic.games/wp-content/uploads/2019/04/Super-Mario-World.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/Super-Mario-World.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd471817",
@@ -21,7 +21,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Konami",
     director: "Masahiro Ueno",
-    image: { url: "https://static-cdn.jtvnw.net/ttv-boxart/Super%20Castlevania%20IV.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/Super%20Castlevania%20IV.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd471819",
@@ -29,7 +29,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Nintendo",
     director: "Yoshio Sakamoto",
-    image: { url: "https://upload.wikimedia.org/wikipedia/pt/thumb/c/cb/Super_Metroid_capa.png/270px-Super_Metroid_capa.png" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/Super_Metroid_capa.png" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181a",
@@ -37,7 +37,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Nintendo",
     director: "Tim Stamper",
-    image: { url: "https://static-cdn.jtvnw.net/ttv-boxart/Donkey%20Kong%20Country.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/Donkey%20Kong%20Country.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181b",
@@ -45,7 +45,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Sega",
     director: "Kouji Inokuchi",
-    image: { url: "https://www.mobygames.com/images/covers/l/14933-ninja-gaiden-nes-front-cover.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/ninja-gaiden-nes-front-cover.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181e",
@@ -53,7 +53,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Blizzard",
     director: "Matthew Findley",
-    image: { url: "https://playclassic.games/wp-content/uploads/2019/04/Blackthorne.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/Blackthorne.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd47181f",
@@ -61,7 +61,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Capcom",
     director: "Yoshinori Takenaka",
-    image: { url: "https://www.retrogamecases.com/wp-content/uploads/snes_mega_manX_front.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/snes_mega_manX_front.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821qw",
@@ -69,7 +69,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Capcom",
     director: "Tokuro Fujiwara",
-    image: { url: "https://playold.games/wp-content/uploads/2018/10/Demons-Crest.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/Demons-Crest.jpg" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821mw",
@@ -77,7 +77,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471818", name: "Platform-adventure" },
     publisher: "Capcom",
     director: "Tokuro Fujiwara",
-    image: { url: "https://upload.wikimedia.org/wikipedia/en/thumb/4/43/Ghouls_and_Ghosts_sales_flyer.png/256px-Ghouls_and_Ghosts_sales_flyer.png" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/Ghouls_and_Ghosts_sales_flyer.png" }
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821rw",
@@ -85,7 +85,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "RPG" },
     publisher: "Square",
     director: "Hironobu Sakaguchi",
-    image: { url: "https://i.pinimg.com/originals/39/61/6a/39616a058f9eb802dbdce9ad2489a510.jpg" } 
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/FFIV.jpg" } 
   },
   {
     _id: "5b21ca3eeb7f6fbccd471821sx",
@@ -93,7 +93,7 @@ const games = [
     genre: { _id: "5b21ca3eeb7f6fbccd471814", name: "RPG" },
     publisher: "Square",
     director: "Takashi Tokita",
-    image: { url: "https://www.mobygames.com/images/covers/l/336839-chrono-trigger-snes-manual.jpg" }
+    image: { url: "https://raw.githubusercontent.com/the-akira/react-games/master/images/chrono-trigger-snes-manual.jpg" }
   }
 ];
 
